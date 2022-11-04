bc29bba9beae1dbe0e5d3a16377039f73edf9bf8
RamiroRojas02 <ramirojasarg@gmail.com>
2022-11-04 12:50:36 -0300

    Fix mt-f1-03

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
-import NavBar from './NavBar'
+import React from "react";
+import NavBar from "./NavBar";
 export default function HeaderComponent(props) {
-  let {click, nav1} = props
+  let { clickNav, nav1, clickUser, userState } = props;
 
   return (
-    <header className='header'>
-    <img className='logo' src="/img/My_Tinerary.png" alt="Logo"></img>
-    <img onClick={click} className="menuNav" src="/img/colapseBar.png" alt="bar"></img>
-    <NavBar naV ={nav1}/>
-    
+    <header className="header">
+      <img className="logo" src="/img/My_Tinerary.png" alt="Logo"></img>
+      <img
+        onClick={clickNav}
+        className="menuNav"
+        src="/img/colapseBar.png"
+        alt="bar"
+      ></img>
+      <img
+        onClick={clickUser}
+        className="user"
+        src="/img/user-circle-solid-240.png"
+        alt="user"
+      ></img>
+      <div className="dropdown">
+        <NavBar naV={nav1} />
+
+        <div className={userState}>
+          <a href="/">Sign in</a>
+          <a href="/">Sign Up</a>
+        </div>
+      </div>
     </header>
-  )
+  );
 }
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -4,15 +4,15 @@ import HeaderComponent from "../components/HeaderComponent";
 import AutoToTop from "../components/AutoToTop";
 import ScrollToTop from "../components/ScrollToTop";
 
-import { Route, Router, Routes, useNavigate } from "react-router-dom";
+
 
 export default function layout(props) {
-  let { menu, state } = props;
+  let { menuEvent, stateNav, userEvent , stateUser } = props;
 
   return (
     <div className="layout">
       <AutoToTop />
-      <HeaderComponent click={menu} nav1={state} />
+      <HeaderComponent clickNav={menuEvent} nav1={stateNav} clickUser= {userEvent} userState={stateUser} />
       {props.children}
       <Footer />
       <ScrollToTop />
