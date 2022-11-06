bec1adc0605045d58c05514163ee9bbd213c941d
RamiroRojas02 <ramirojasarg@gmail.com>
2022-11-06 16:38:40 -0300

    Pagina Sign Up

--- src/ComponentRoutes.jsx
+++ src/ComponentRoutes.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import  {Routes,Route} from "react-router-dom"
 import NotFound from './components/NotFound'
-import Home from './layout/Home'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
 export  const ComponentRoutes=()=> {
   return (
     <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/*" element={<NotFound/>}/>
+        <Route path="/SignUp" element={<SignUp/>}/>
     </Routes>
   )
 }
